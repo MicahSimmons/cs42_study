739bc71fbfce98745580d9bd7570abbc1c2599b5
Micah Simmons <sonet_boy@yahoo.com>
2022-09-02 11:24:17 -0700

    Simple guessing game

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cs42_study
+# week 2, adding some User input examples!
--- scripts/script.js
+++ scripts/script.js
@@ -2,103 +2,171 @@
 
 console.log("Hello World!");
 
+/* Events */
+var Events = {
+    START: 0,
+    GUESS: 1,
+    CORRECT: 2,
+    INCORRECT: 3
+}
+
+/* States */
+var States = {
+    START: 0,
+    PLAYING: 1,
+    GAMEOVER: 2
+}
+
 /* Model */
-var model = {
-    theNumber: -1,
-    guesses: 0,
-    lastGuess: -1
-}
-
-/* Model Init */
-function initModel () {
-    model.theNumber = Math.floor(Math.random() * 100) + 1
-    model.guesses = 0
-    model.lastGuess = -1
-}
-
-var events = {
-    NONE: -2,
-    INVALID: -1,
-    CORRECT: 0,
-    TOO_HIGH: 1,
-    TOO_LOW: 2
-}
-function guess (new_guess) {
-    model.guesses++;
-    model.lastGuess = new_guess;
-
-    if (new_guess == model.theNumber) {
-      return events.CORRECT;
+class Model {
+    constructor () {
+        this.gameState = States.START;
+        this.theNumber = -1;
+        this.guesses = 0;
+        this.lastGuess = -1;        
     }
-  
-    if (new_guess < model.theNumber) {
-      return events.TOO_LOW;
+
+    init () {
+        this.theNumber = Math.floor(Math.random() * 100) + 1
+        this.guesses = 0
+        this.lastGuess = -1        
     }
-  
-    if (new_guess > model.theNumber) {
-      return events.TOO_HIGH;
+
+    guess (new_guess) {
+        this.guesses++;
+        this.lastGuess = new_guess;
+    
+        if (new_guess == this.theNumber) {
+          return Events.CORRECT;
+        } else {
+          return Events.INCORRECT;
+        }
     }
 }
+
 
 /* View Interface */
-var gameOut = document.getElementById('#output')
-var gameIn = document.getElementById("#gameInput")
-var gameBtn = document.getElementById("#gameBtn")
+class View {
+    constructor (root) {
+        console.log("here!");
+        this.root = root;
+        this.gameOut = document.getElementById('#output')
+        this.gameIn = document.getElementById("#gameInput")
+        this.gameBtn = document.getElementById("#gameBtn")
+        this.btn_cb = null;
+        this.last_guess_count = 0;
 
-function update (model, event) {
-    switch (event) {
-        case events.NONE:
-            gameOut.innerHTML += "Type a number between 1 and 100!<br>";
-            break;
-        case events.INVALID:
-            gameOut.innerHTML += "That wasn't a number...<br>";
-            break;
-        case events.CORRECT:
-            gameOut.innerHTML += "You got it in " + model.guesses + " tries!<br>";
-            break;
-        case events.TOO_HIGH:
-            gameOut.innerHTML += model.lastGuess + " was too high...<br>"
-            break;
-        case events.TOO_LOW:
-            gameOut.innerHTML += model.lastGuess + " was too low...<br>"
-            break;
+        this.btnHandler = this.btnHandler.bind(this);
+        this.update = this.update.bind(this);
+
+        console.log(this.gameBtn);
+        this.gameBtn.addEventListener("click", this.btnHandler);
+    }
+
+    addButtonListener( btn_cb ) {
+        this.btn_cb = btn_cb;
+    }
+
+    btnHandler () {
+        var inputText = this.gameIn.value;
+        if (this.btn_cb) {
+            this.btn_cb(parseInt(inputText));
+        }
+    }
+
+    update (model, event) {
+        if (model.guesses == this.last_guess_count) {
+            return;
+        }
+        this.last_guess_count = model.guesses;
+
+        if (model.lastGuess == "") {
+          this.gameOut.innerHTML += "Type a number between 1 and 100!<br>";
+        }
+
+        if (isNaN(model.lastGuess)) {
+            this.gameOut.innerHTML += "That wasn't a number...<br>";
+        }
+
+        if (model.lastGuess == model.theNumber) {
+            this.gameOut.innerHTML += "You got it in " + model.guesses + " tries!<br>";
+        }
+
+        if (model.lastGuess < model.theNumber) {
+            this.gameOut.innerHTML += model.lastGuess + " was too low...<br>"
+        }
+
+        if (model.lastGuess > model.theNumber) {
+            this.gameOut.innerHTML += model.lastGuess + " was too high...<br>"
+        }
     }
 }
 
-/* Controls */
-gameBtn.addEventListener("click", () => {
-  var inputText = gameIn.value;
-  var result;
 
-  while (1) {
-    /* Check for empty data */
-    if (inputText == "") {
-        result = events.NONE;
-        break;
-    } 
-    
-    /* Check for bad data */
-    if (isNaN(inputText)) {
-        result = events.INVALID;
-        break;
+/* Control Logic */
+class Control {
+    constructor () {
+        this.fsm = this.fsm.bind(this);
+        this.guessHandler = this.guessHandler.bind(this);
+        this.setView = this.setView.bind(this);
     }
 
-    /* Test the Result */
-    result = guess(inputText);
-    break;
-  }
+    setView (view) {
+        this.view = view;
+    }
 
-  /* Update the display */
-  update(model, result);
-});
+    fsm(model, event, data) {
+        switch (model.gameState) {
+            case States.START:
+                switch (event) {
+                    case Events.START:
+                        model.init();
+                        model.gameState = States.PLAYING;
+                        break;
+                }
+                break;
+
+            case States.PLAYING:
+                switch (event) {
+                    case Events.GUESS: {
+                        var result = model.guess(data);
+                        this.fsm(model, result, data);
+                    } break;
+                    case Events.CORRECT:
+                        model.gameState = States.GAMEOVER;
+                        break;
+                    case Events.INCORRECT:
+                        model.gameState = States.PLAYING;
+                        break;
+                }
+                break;
+            case States.GAMEOVER:
+                break;
+        }
+
+        this.view.update(model, event);
+    }
+    
+    guessHandler( guessText ) {
+        q(Events.GUESS, guessText);
+    }
+}
+
 
 
 /* Main */
-initModel();
+var model = new Model();
+model.init();
 
+var control = new Control();
 
-x=24;
-while (x>=0) {
-   console.log(x);
-   x = x - 1;
+var gameroot = document.getElementById("#gamecontainer");
+var view = new View(gameroot)
+view.addButtonListener(control.guessHandler);
+control.setView(view);
+
+function q(event, data) {
+    setTimeout(control.fsm, 0, model, event, data);
 }
+
+control.fsm(model, Events.START, {});
